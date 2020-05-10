1af031389efddc6aa01f426c068e63f36e443646
Erik Miller <eamiller34@gmail.com>
2020-05-10 11:26:20 -0700

    First commit

---
+++ firebase-to-dos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firebase-to-dos
+//
+//  Created by Erik Miller on 5/10/20.
+//  Copyright © 2020 Erik Miller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
